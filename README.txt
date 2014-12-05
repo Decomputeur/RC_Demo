fe4eb6430ee200ccdf332c4fe23e70c94bf73a97
eagle00789 <info@decomputeur.nl>
2014-12-05 23:35:18 +0100

    Some more explained

--- README.txt
+++ README.txt
@@ -1,12 +1,16 @@
 /**
  * Demo Plugin
  *
- * Plugin that adds a new tab to the settings section to create client-side e-mail filtering.
+ * This plugin show how to make a plugin for roundcube by showing the basics (and some advanced stuff).
  *
  * @version 1.0.0
  * @author Chris Simon <info@decomputeur.nl> from version 2.1.3
  *  
  */
+
+Extra Information
+-----------------
+Just make sure that when you create your own plugin, that you name the folder and the primary .php file exactly the same as can be seen in this demo plugin.
 
 Setup
 -----
@@ -16,3 +20,4 @@ History
 -------
 
 1.0 Initial version.
+
--- demo_plugin.php
+++ demo_plugin.php
@@ -18,9 +18,11 @@ class filters extends rcube_plugin{
 	/**
 	 * Here we define in what parts of the roundcube app we are going to hook our plugin into
 	 * Valid choices are: login logout mail addressbook settings
-	 * To use multiple tasks for a single plugin, seperate them with a pipe symbol
 	 *
-	 *For demo purposes, we use only settings in our demo
+	 * To use multiple tasks for a single plugin, seperate them with a pipe symbol.
+	 * For example: login|mail|settings
+	 *
+	 * For demo purposes, we use only settings in our demo
 	 */
   public $task = 'settings';
 	/**
@@ -35,7 +37,10 @@ class filters extends rcube_plugin{
 	 * This is the main function of the plugin that gets called when the plugin is initialized.
 	 */
   function init(){
-	/** First we need to get a instance to the current working roundcube in which out plugin is being loaded */
+	/**
+	 * First we need to get a instance to the current working roundcube in which out plugin is being loaded
+	 * Now $this->rc holds all global roundcube items
+	 */
     $this->rc = rcmail::get_instance();	
 	/**
 	 * Next we load our configuration file.
@@ -44,7 +49,21 @@ class filters extends rcube_plugin{
 	 */
 	$this->load_config('config.inc.php.dist');
 	$this->load_config('config.inc.php');
-	/** Next we load our own languagefiles for our custom strings */
+	/**
+	 * Next we load our own languagefiles for our custom strings
+	 */
     $this->add_texts('localization/');
+	/**
+	 * Now we can do any of the following items:
+	 * Add hooks: $this->add_hook('some_hook', array($this, 'name_of_internal_function'));
+	 * Register Actions: $this->register_action('plugin.someaction', array($this, 'name_of_internal_function'));
+	 * or your own stuff that you want to do in the init section of the plugin.
+	 * For this demo, we are going to add an item to the settings page.
+	 */
+	 $this->register_action('plugin.demo-plugin', array($this, 'demo_init'));
+	 /**
+	  * Now we register our plugin into the Roundcube instance using the label
+	  */
+	 $this->rc->output->add_label('demo_plugin');
   }
 }
